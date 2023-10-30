eb2c39d42958ea41fd911e484dc455bff5669a7c
Anthony Robin <contact@anthony-robin.fr>
2023-10-30 17:15:10 +0100

    New profile feature
    Add a new page to handle profile configuration such as firstname, lastname,
    email, phone, photo, ...

---
+++ app/controllers/me/profiles_controller.rb
@@ -0,0 +1,37 @@
+module Me
+  class ProfilesController < ApplicationController
+    before_action :require_login
+
+    # @route GET /me/profile (me_profile)
+    def show
+      authorize! current_user, with: ProfilePolicy
+    end
+
+    # @route GET /me/profile/edit (edit_me_profile)
+    def edit
+      authorize! current_user, with: ProfilePolicy
+    end
+
+    # @route PATCH /me/profile (me_profile)
+    # @route PUT /me/profile (me_profile)
+    def update
+      authorize! current_user, with: ProfilePolicy
+
+      current_user.photo.purge if user_params[:remove_photo] == '1'
+
+      if current_user.update(user_params)
+        redirect_to me_profile_path, notice: 'Votre profil a bien été mis à jour'
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(
+        :first_name, :last_name, :email, :photo, :remove_photo
+      )
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new create]
 
+  namespace :me do
+    resource :profile, only: %i[show edit update]
+  end
+
   resources :customers do
     get :search
     member do
